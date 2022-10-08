62e3fe4315d5e3d05ab055e664033765c8ccf80f
Sebastian Coronel <scoronelhamilton@gmail.com>
2022-10-08 16:26:09 -0500

    remove yarn lock, add event types

---
+++ src/components/bill/EventsTable.tsx
@@ -0,0 +1,5 @@
+import { Event } from 'types/types'
+
+export default function EventsTable({ events }: { events: Event[] }) {
+  return <div></div>
+}
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,16 +1,27 @@
 /* eslint-disable no-unused-vars */
 
-export interface Bill {
-  id: string;
-  value: string;
-  wordHash: string;
-  codeHash: string;
-  isFunded: boolean;
-  isRedeemed: boolean;
+export enum BillStatus {
+  Unfunded = 'Unfunded',
+  Funded = 'Funded',
+  Redeemed = 'Redeemed'
 }
 
-export enum BillStatus {
-  Unfunded = "Unfunded",
-  Funded = "Funded",
-  Redeemed = "Redeemed",
+export enum EventType {
+  Request = 'Request',
+  Issue = 'Issue',
+  Fund = 'Fund',
+  Redeem = 'Redeem'
+}
+
+export interface Bill {
+  id: string
+  value: string
+  wordHash: string
+  codeHash: string
+  isFunded: boolean
+  isRedeemed: boolean
+}
+export interface Event {
+  billId: string
+  type: EventType
 }
