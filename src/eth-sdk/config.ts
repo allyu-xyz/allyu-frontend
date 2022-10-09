d57288d157702f7496cd2e233ab3952b2ab55920
Sebastian Coronel <scoronelhamilton@gmail.com>
2022-10-09 08:26:06 -0500

    abi touches

--- src/eth-sdk/config.ts
+++ src/eth-sdk/config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
     polygonMumbai: {
       dai: '0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F',
       allyu: '0x12c2c185f8B14006aadE9f8914508C49CE019A84'
+    },
+    optimismGoerli: {
+      dai: '0xd0B806F06E77157c3f39182A2C0b4C05A33D84a5',
+      allyu: '0xdbf20E8AaA24660fb4e566d5D6FF47E726E5B13b'
     }
   }
 })
